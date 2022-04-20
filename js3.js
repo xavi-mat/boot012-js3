157a0b7deafed03daaf9b5fc360dbabdc2cf1606
Xavimat <xaviermatoses@gmail.com>
2022-04-20 16:57:07 +0200

    Last functions
    isPrime, checkPassword, inverseCalculator.
    Format document

--- js3.js
+++ js3.js
@@ -305,8 +305,8 @@ console.log("\nconvierteString");
 
 const convierteString = (num) =>
     isNaN(num) ?
-    'Debo ser ejecutada con un número' :
-    num.toString();
+        'Debo ser ejecutada con un número' :
+        num.toString();
 
 console.log('4', convierteString('4'));
 console.log(4, convierteString(4));
@@ -321,7 +321,7 @@ console.log("\ncaracterInicial");
 const caracterInicial = (s) =>
     typeof s === 'string' ?
         s.length > 0 ? s[0] : 'Debo ser ejecutada con un string no vacío' :
-    'Debo ser ejecutada con un string';
+        'Debo ser ejecutada con un string';
 
 console.log('Hola', caracterInicial('Hola'));
 
@@ -337,7 +337,7 @@ console.log("\nÚltimo carácter");
 const ultimoCaracter = (s) =>
     typeof s === 'string' ?
         s.length > 0 ? s.slice(-1) : 'Debo ser ejecutada con un string no vacío' :
-    'Debo ser ejecutada con un string';
+        'Debo ser ejecutada con un string';
 
 console.log('Hola', ultimoCaracter('Hola'));
 
@@ -353,7 +353,7 @@ console.log("\ncuentaCaracteres");
 const cuentaCaracteres = (s) =>
     typeof s === 'string' ?
         s.length > 0 ? s.length : 'Debo ser ejecutada con un string no vacío' :
-    'Debo ser ejecutada con un string';
+        'Debo ser ejecutada con un string';
 
 console.log('Hola', cuentaCaracteres('Hola'));
 
@@ -369,7 +369,7 @@ const getCiudadesOrdenada = (ciudades) => {
     if (typeof ciudades === 'string') {
         ciudades = ciudades.split(',');
         if (ciudades.length > 1) {
-            return ciudades.sort((a,b)=>a.localeCompare(b));
+            return ciudades.sort((a, b) => a.localeCompare(b));
         }
     }
     return 'no es un formato correcto';
@@ -386,12 +386,12 @@ console.log(
 // por ejemplo orejero, rallar o somos.  Crea la función esPalindromo que recibirá
 // una cadena de texto y deberá devolver si es un palíndromo o no.
 console.log("\nesPalindromo:");
-const esPalindromo = (pal, start=0, end=null) => {
+const esPalindromo = (pal, start = 0, end = null) => {
     if (typeof pal !== 'string' || pal.length === 0) {
         return 'no es un formato correcto';
     }
-    if (end===null) {
-        end = pal.length-1;
+    if (end === null) {
+        end = pal.length - 1;
     }
     if (end <= start) {
         return 'SÍ es palíndromo';
@@ -399,7 +399,7 @@ const esPalindromo = (pal, start=0, end=null) => {
     if (pal[start] !== pal[end]) {
         return 'no es palíndromo';
     }
-    return esPalindromo(pal, start+1, end-1);
+    return esPalindromo(pal, start + 1, end - 1);
 };
 
 // Si la función no recibe una cadena de texto o está vacía 'no es un formato
@@ -415,8 +415,8 @@ console.log("entendederas", esPalindromo("entendederas"));
 console.log("\ngetPrecioMostrar");
 const getPrecioMostrar = (num) =>
     !isNaN(Number(num)) ?
-    (num*1).toFixed(2) + ' €' :
-    'no es un formato correcto';
+        (num * 1).toFixed(2) + ' €' :
+        'no es un formato correcto';
 
 console.log("getPrecioMostrar(2)", getPrecioMostrar(2));
 console.log("getPrecioMostrar('5.5')", getPrecioMostrar('5.5'));
@@ -432,12 +432,12 @@ console.log("getPrecioMostrar('56.754')", getPrecioMostrar('56.754'));
 console.log("\nDivisión");
 const division = (dividendo, divisor) =>
     !isNaN(Number(dividendo)) && !isNaN(Number(divisor)) ?
-    dividendo / divisor :
-    'no son dos números correctos';
+        dividendo / divisor :
+        'no son dos números correctos';
 
-console.log("division(4,5)", division(4,5));
-console.log("division('4',5)", division('4',5));
-console.log("division('x',5)", division('x',5));
+console.log("division(4,5)", division(4, 5));
+console.log("division('4',5)", division('4', 5));
+console.log("division('x',5)", division('x', 5));
 
 //  Crea la función esPar que acepte como argumento un número y devuelva true si
 // es par y false si es impar
@@ -454,62 +454,104 @@ console.log("esPar('YY')", esPar('YY'));
 // y devuelva un array ordenado de mayor a menor
 console.log("\nordenarArray2");
 const ordenarArray2 = (array) =>
-    array.every((e)=>!isNaN(e)) ?
-    array.sort((a,b)=>Number(b)-Number(a)) :
-    null;
+    array.every((e) => !isNaN(e)) ?
+        array.sort((a, b) => Number(b) - Number(a)) :
+        null;
 
-console.log("ordenarArray2([1,5,2,4,3])", ordenarArray2([1,5,2,4,3]));
-console.log("ordenarArray2([1,5,'2',4,3])", ordenarArray2([1,5,'2',4,3]));
-console.log("ordenarArray2([1,5,'x',4,3])", ordenarArray2([1,5,'x',4,3]));
+console.log("ordenarArray2([1,5,2,4,3])", ordenarArray2([1, 5, 2, 4, 3]));
+console.log("ordenarArray2([1,5,'2',4,3])", ordenarArray2([1, 5, '2', 4, 3]));
+console.log("ordenarArray2([1,5,'x',4,3])", ordenarArray2([1, 5, 'x', 4, 3]));
 
 // Crear la función obtenerImpares que acepta como argumento un array de números
 // y devuelva un array con los elementos impares
 console.log("\nobtenerImpares");
-const obtenerImpares = (array) => array.filter((num)=>!esPar(num));
+const obtenerImpares = (array) => array.filter((num) => !esPar(num));
 
-console.log("obtenerImpares([1,5,2,4,3])", obtenerImpares([1,5,2,4,3]));
-console.log("obtenerImpares([4,6,8])", obtenerImpares([4,6,8]));
-console.log("obtenerImpares([3,5,7,9])", obtenerImpares([3,5,7,9]));
+console.log("obtenerImpares([1,5,2,4,3])", obtenerImpares([1, 5, 2, 4, 3]));
+console.log("obtenerImpares([4,6,8])", obtenerImpares([4, 6, 8]));
+console.log("obtenerImpares([3,5,7,9])", obtenerImpares([3, 5, 7, 9]));
 
 // Crear la función sumarArray que acepte como argumento un array numérico y
 // devuelva la suma de los números en el array Array: [1, 2, 3] resultado: 6
 console.log("\nsumarArray");
-const sumarArray = (array) => array.reduce((total, num)=>total+num);
+const sumarArray = (array) => array.reduce((total, num) => total + num);
 
-console.log("sumarArray([1,2,3,4])", sumarArray([1,2,3,4]));
-console.log("sumarArray([1,2,3,4,5,6,7,8,9,10])", sumarArray([1,2,3,4,5,6,7,8,9,10]));
+console.log("sumarArray([1,2,3,4])", sumarArray([1, 2, 3, 4]));
+console.log("sumarArray([1,2,3,4,5,6,7,8,9,10])", sumarArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
 // Crear la función multiplicarArray que acepte como argumento un array numérico
 // y devuelva la multiplicación de los números en el array Array: [2, 3, 4] resultado: 24
 console.log("\nmultiplicarArray");
-const multiplicarArray = (array) => array.reduce((total, num)=>total*num);
+const multiplicarArray = (array) => array.reduce((total, num) => total * num);
 
-console.log("sumarArray([1,2,3,4])", multiplicarArray([1,2,3,4]));
+console.log("sumarArray([1,2,3,4])", multiplicarArray([1, 2, 3, 4]));
 console.log(
     "sumarArray([1,2,3,4,5,6,7,8,9,10])",
-    multiplicarArray([1,2,3,4,5,6,7,8,9,10])
+    multiplicarArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 );
 
 // Crea una función que dada una array de números, devuelve una nueva array que
 // tenga solo los números que son 5 o más. Ejemplo entrada [3, 6, 8, 2] salida [6, 8]
 console.log("\ncincoOMas");
-const cincoOMas = (array) => array.filter()
+const cincoOMas = (array) => array.filter((num) => num >= 5);
+
+console.log("cincoOMas([3,6,8,2])", cincoOMas([3, 6, 8, 2]));
 
 // Crea una aplicación que nos pida un número por prompt y con un método se lo
 // pasamos por parámetro para que nos indique si es o no un número primo, debe
 // devolver true si es primo sino false.
-
-
 // Un número primo es aquel que solo puede dividirse entre 1 y sí mismo. Por
 // ejemplo: 25 no es primo, ya que 25 es divisible entre 5, sin embargo, 17 si es primo.
+console.log("\nNúmero primo: pulsa el botón o ejecuta en la consola 'maybePrime()'.");
+
+const maybePrime = () => {
+    console.log("Introduce un número en el prompt...");
+    const num = prompt("Introduce un número y te diré si es primo");
+    if (isNaN(num)) {
+        console.log("No es un número válido.");
+        return;
+    }
+
+    const checkPrime = (num, check = 2) => {
+        if (check > Math.sqrt(num)) { return true; }
+        if (num % check === 0) { return false; }
+        return checkPrime(num, check + 1);
+    }
+    console.log(`¿Es primo ${num}?`, checkPrime(num));
+};
 
 
-// Crea una función checkPasswordcon una variable tipo String que contenga una
+// Crea una función checkPassword con una variable tipo String que contenga una
 // contraseña cualquiera. Después se te pedirá que introduzcas la contraseña (usando
 //     prompt), con 3 intentos. Cuando aciertes ya no pedirá más la contraseña y
 //     mostrará un mensaje diciendo “Enhorabuena”(ej.un alert). Piensa bien en la
 //     condición de salida (3 intentos y si acierta sale, aunque le queden intentos).
+console.log("\ncheckPassword: Click button or execute 'checkPasswordApp()'");
 
+const password = '12345678';
+
+const checkPasswordApp = () => {
+    const checkPassword = (str) =>
+        prompt("Input password") === str;
+    const MAX_TRIES = 3;
+    let tries = 0;
+    let loggedIn = false;
+
+    while (tries < MAX_TRIES && !loggedIn) {
+        if (checkPassword(password)) {
+            loggedIn = true;
+        } else {
+            tries++;
+        }
+    }
+    if (loggedIn) {
+        console.log("Congratulations!");
+        alert("Congratulations!");
+    } else {
+        console.log("No more tries.");
+        alert("No more tries.");
+    }
+};
 
 // Crea una función llamada CalculadoraInversa, nos pedirá 2 operandos (int) y un signo aritmético (String), según este último se realizará la operación correspondiente. Al final mostrará el resultado en un cuadro de diálogo. Los signos aritméticos disponibles son:
 // +: suma los dos operadores.
@@ -518,7 +560,48 @@ const cincoOMas = (array) => array.filter()
 // /: divide los operandos, este debe dar un resultado con decimales (double)
 // ^: 1o operando como base y 2o como exponente.
 // %: módulo, resto de la división entre operando1 y operando2.
+console.log("\ncalculadoraInversa: ");
+const calculadoraInversa = () => {
+    const operand1 = prompt("Input first operand:");
+    const operand2 = prompt("Input second operand:");
+    const operator = prompt("Input operator (+ - * / ^ %):");
 
+    let result = `${operand1} ${operator} ${operand2} = `;
+
+    if (isNaN(operand1)) {
+        result += 'OPERAND1 IS INVALID. ';
+    }
+    if (isNaN(operand2)) {
+        result += 'OPERAND2 IS INVALID. ';
+    }
+    if (!isNaN(operand1) && !isNaN(operand2)) {
+        switch (operator) {
+            case '+':
+                result += operand1 + operand2;
+                break;
+            case '-':
+                result += operand1 - operand2;
+                break;
+            case '*':
+                result += operand1 * operand2;
+                break;
+            case '/':
+                result += operand1 / operand2;
+                break;
+            case '^':
+                result += operand1 ** operand2;
+                break;
+            case '%':
+                result += operand1 % operand2;
+                break;
+            default:
+                result += 'INVALID OPERATOR';
+                break;
+        }
+    }
+    console.log(result);
+    alert(result);
+};
 
 
 // ////////////////////////////////////////////////////////////////////////////////
